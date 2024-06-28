2c98a23e6615741072843c22c630763bd146e2c8
didier-tp <d>
2024-06-28 11:33:52 +0200

    s

--- tp_springBoot_juin2024/appliSpringBoot/src/main/java/tp/appliSpringBoot/rest/CompteRestCtrl.java
+++ tp_springBoot_juin2024/appliSpringBoot/src/main/java/tp/appliSpringBoot/rest/CompteRestCtrl.java
@@ -1,6 +1,8 @@
 package tp.appliSpringBoot.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,10 +16,25 @@ public class CompteRestCtrl {
 
     @Autowired
     private CompteRepository compteRepository;
-
+/*
     @GetMapping("/{numero}" )
     public Compte getCompteByNum(@PathVariable("numero") Long numero) {
         return compteRepository.findById(numero).get();  //V1
     }
+*/
+    @GetMapping("/{numero}" )
+    public ResponseEntity<Compte> getCompteByNum(@PathVariable("numero") Long numero) {
+        if(compteRepository.findById(numero).isEmpty())
+//          return ResponseEntity.notFound().build();
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        else
+            return ResponseEntity.ok(compteRepository.findById(numero).get());
+    }
+
+    /*
+    @GetMapping("" )
+    public List<Compte> getComptesByCriteria(....) {
+        return compteRepository.findById(numero).get();  //V1
+    }*/
 
 }
