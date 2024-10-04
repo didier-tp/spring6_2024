377e77e70ac8a08f020af4c8d08a69d9f38fa9c0
DidierDefrance <didier@d-defrance.fr>
2024-10-04 14:25:02 +0200

    s

--- tp/octobre2024/debutAppliSpring/src/main/java/tp/appliSpring/web/rest/CompteRestCtrl.java
+++ tp/octobre2024/debutAppliSpring/src/main/java/tp/appliSpring/web/rest/CompteRestCtrl.java
@@ -8,15 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
 import tp.appliSpring.converter.GenericMapper;
@@ -28,6 +20,9 @@ import tp.appliSpring.dto.CompteDto;
 
 @RestController //@Component de type controller d'api rest
 @RequestMapping(value="/rest/api-bank/comptes" , headers="Accept=application/json")
+@CrossOrigin(origins = "*" ,
+		methods = { RequestMethod.GET , RequestMethod.POST ,
+				RequestMethod.PUT , RequestMethod.DELETE})
 public class CompteRestCtrl {
 	
 	@Autowired
