0837ae032711adf588a785bf90cd368a64cdc5f0
didier-tp <d2f.defrance@gmail.com>
2024-11-21 10:40:27 +0100

    debut security

--- tp/novembre2024/debutAppliSpring/src/main/java/tp/appliSpring/AppliSpringApplication.java
+++ tp/novembre2024/debutAppliSpring/src/main/java/tp/appliSpring/AppliSpringApplication.java
@@ -24,6 +24,7 @@ public class AppliSpringApplication extends SpringBootServletInitializer {
 		if(profilsActifs==null || profilsActifs.equals("")) {
 			//valeur par défaut si pas encore précisé
 			System.setProperty("spring.profiles.default", "dev,reInit");
+			//System.setProperty("spring.profiles.default", "dev,reInit,withSecurity");
 			//System.setProperty("spring.profiles.default", "DEV"); //avec H2 et reInit
 			//System.setProperty("spring.profiles.default", "DEV2"); //avec mysql et reInit
 			//System.setProperty("spring.profiles.active", "dev2,reInit");
