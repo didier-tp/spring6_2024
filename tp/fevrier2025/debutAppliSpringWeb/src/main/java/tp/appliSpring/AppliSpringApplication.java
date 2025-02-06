97f913a0d602726e2a9814253d206a5bdd792a43
didier Defrance <didier@d-defrance.fr>
2025-02-06 15:39:12 +0100

    s

--- tp/fevrier2025/debutAppliSpringWeb/src/main/java/tp/appliSpring/AppliSpringApplication.java
+++ tp/fevrier2025/debutAppliSpringWeb/src/main/java/tp/appliSpring/AppliSpringApplication.java
@@ -1,10 +1,15 @@
 package tp.appliSpring;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class AppliSpringApplication {
+	
+	private static Logger logger = LoggerFactory.getLogger(AppliSpringApplication.class);
 	
 	
 	public static void initProfiles() {
@@ -13,8 +18,8 @@ public class AppliSpringApplication {
 		if(profilsActifs!=null) {
 			System.out.println("spring.profiles.active="+profilsActifs);
 		}else {
-			String defaultProfils  = "dev,reInit";
-			//String defaultProfils  = "dev,reInit,withSecurity";
+			//String defaultProfils  = "dev,reInit";
+			String defaultProfils  = "dev,reInit,withSecurity";
 			//String defaultProfils  = "dev2,reInit";
 			//String defaultProfils  = "prod";
 			System.setProperty("spring.profiles.default", defaultProfils);
@@ -26,7 +31,8 @@ public class AppliSpringApplication {
 		initProfiles();
 		SpringApplication.run(AppliSpringApplication.class, args);
 		//url de l'appli
-		System.out.println("http://localhost:8181/appliSpring");
+		//System.out.println("http://localhost:8181/appliSpring");
+		logger.info("http://localhost:8181/appliSpring");
 	}
 
 }
