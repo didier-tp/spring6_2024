e14ff274a5fad9f9e2b7a150e3b35dcf24adc67d
Didier_Defrance <didier@d-defrance.fr>
2025-03-10 18:45:06 +0100

    appliSpringV3 et debutAppliSpringWeb sans lombok (pour éviter pb eclipse)

--- tp/debutAppliSpringWeb/src/main/java/tp/appliSpring/bank/core/model/Operation.java
+++ tp/debutAppliSpringWeb/src/main/java/tp/appliSpring/bank/core/model/Operation.java
@@ -1,17 +1,17 @@
 package tp.appliSpring.bank.core.model;
 
 import tp.appliSpring.generic.model.WithId;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//import lombok.Setter;
+//import lombok.ToString;
 
 import java.util.Date;
 
-@ToString
-@NoArgsConstructor
-@Getter
-@Setter
+//@ToString
+//@NoArgsConstructor
+//@Getter
+//@Setter
 public class Operation implements WithId<Long> {
     private Long numero;
 
@@ -35,5 +35,51 @@ public class Operation implements WithId<Long> {
 
     public Operation(Long numero, String label, Double montant){
         this(numero,label,montant,new Date());
+    }
+
+    public Operation() {
+        this(null,null,null);
+    }
+
+    @Override
+    public String toString() {
+        return "Operation{" +
+                "numero=" + numero +
+                ", label='" + label + '\'' +
+                ", montant=" + montant +
+                ", dateOp=" + dateOp +
+                '}';
+    }
+
+    public Long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Long numero) {
+        this.numero = numero;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public Double getMontant() {
+        return montant;
+    }
+
+    public void setMontant(Double montant) {
+        this.montant = montant;
+    }
+
+    public Date getDateOp() {
+        return dateOp;
+    }
+
+    public void setDateOp(Date dateOp) {
+        this.dateOp = dateOp;
     }
 }
