cc78d985644e41df51b6a3f319efe40ba1ad40cb
Didier_Defrance <didier@d-defrance.fr>
2025-03-12 08:03:49 +0100

    debut de partie thymeleaf dans appliSpringV3

--- tp/appliSpringV3/src/main/java/tp/appliSpring/bank/core/init/ReInitDefaultDataSet.java
+++ tp/appliSpringV3/src/main/java/tp/appliSpring/bank/core/init/ReInitDefaultDataSet.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import jakarta.annotation.PostConstruct;
+import tp.appliSpring.bank.core.model.Client;
+import tp.appliSpring.bank.core.service.ServiceClient;
 import tp.appliSpring.bank.core.service.ServiceCompte;
 import tp.appliSpring.bank.core.service.ServiceOperation;
 import tp.appliSpring.bank.core.model.Compte;
@@ -21,6 +23,9 @@ public class ReInitDefaultDataSet {
 	
 	@Autowired
 	ServiceOperation serviceOperation;
+
+	@Autowired
+	ServiceClient serviceClient;
 	
 	@PostConstruct
 	public void intialiserJeuxDeDonnees() {
@@ -31,8 +36,16 @@ public class ReInitDefaultDataSet {
 		serviceOperation.create(new Operation(null,"achat 2" , -6.0 , new Date()),
 				                             cptA.getNumero());
 		Compte cptB = serviceCompte.create(new Compte(null,"compteB",200.0));
+
+		Client client1 = new Client(null, "alex", "Therieur", "12 rue Elle 75001 Paris", "email1");
+		serviceClient.create(client1);
+
 		Compte cptC = serviceCompte.create(new Compte(null,"compteC",20.0));
 		Compte cptD = serviceCompte.create(new Compte(null,"compteD",30.0));
+
+		Client client2 = new Client(null, "axelle", "Aire", "13 rue Elle 75001 Paris", "email2");
+		serviceClient.create(client2);
+
 	}
 
 }
