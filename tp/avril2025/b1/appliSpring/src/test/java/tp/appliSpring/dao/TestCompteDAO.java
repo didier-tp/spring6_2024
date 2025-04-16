83e6458b540130cad8f157d4bae52e6e14729bec
didier Defrance <didier@d-defrance.fr>
2025-04-16 16:21:50 +0200

    s

--- tp/avril2025/b1/appliSpring/src/test/java/tp/appliSpring/dao/TestCompteDAO.java
+++ tp/avril2025/b1/appliSpring/src/test/java/tp/appliSpring/dao/TestCompteDAO.java
@@ -1,6 +1,9 @@
 package tp.appliSpring.dao;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -20,6 +23,19 @@ public class TestCompteDAO {
 	
 	@Autowired
 	private CompteDAO compteDAO;//composant à tester (pris en charge par spring)
+	
+	
+	@Test
+	public void testRechercherCompteViaSoldeMini() {
+		compteDAO.save(new Compte(null,"compteX1",500.0));
+		compteDAO.save(new Compte(null,"compteX2",600.0));
+		compteDAO.save(new Compte(null,"compteX3",700.0));
+		List<Compte> comptesAvecSoldeAuMoins600 = compteDAO.findBySoldeGreaterThanEqual(600.0);
+		//List<Compte> comptesAvecSoldeAuMoins600 = compteDAO.findSelonSoldeMini(600.0);
+		assertTrue(comptesAvecSoldeAuMoins600.size()>=2);
+		logger.debug("comptesAvecSoldeAuMoins600="+comptesAvecSoldeAuMoins600);
+		
+	}
 	
 	@Test
 	public void testCrudCompte() {
