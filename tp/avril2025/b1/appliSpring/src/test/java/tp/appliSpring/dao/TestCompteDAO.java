e6906079d74c44be2cb2dd91a35c6872ba0db62c
didier Defrance <didier@d-defrance.fr>
2025-04-16 15:48:18 +0200

    s

--- tp/avril2025/b1/appliSpring/src/test/java/tp/appliSpring/dao/TestCompteDAO.java
+++ tp/avril2025/b1/appliSpring/src/test/java/tp/appliSpring/dao/TestCompteDAO.java
@@ -1,24 +1,30 @@
 package tp.appliSpring.dao;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 import tp.appliSpring.entity.Compte;
 
-@SpringBootTest
+@SpringBootTest //extension Spring pour Junit
+//démarre un test en reprenant la configuration de la classe principale (celle où il y a main())
 @ActiveProfiles("dev")//pour prendre en compte application-dev.properties
 public class TestCompteDAO {
 	
+	Logger logger = LoggerFactory.getLogger(TestCompteDAO.class);
+	
 	@Autowired
-	private CompteDAO compteDAO;//à tester
+	private CompteDAO compteDAO;//composant à tester (pris en charge par spring)
 	
 	@Test
 	public void testCrudCompte() {
 		Compte cptA = new Compte(null,"compteA",50.0);
-		compteDAO.save(cptA);//sauvegarder le compte en base de données
-		//...
+		Compte cptASauvegarde = compteDAO.save(cptA);//sauvegarder le compte en base de données
+		logger.debug("cptASauvegarde="+cptASauvegarde);
+		
 	}
 
 }
