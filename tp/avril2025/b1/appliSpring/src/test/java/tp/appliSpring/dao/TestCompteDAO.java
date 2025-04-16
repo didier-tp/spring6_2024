fb26ace3446c63d65c1d980d2c53c8f8cb3f63ee
didier Defrance <didier@d-defrance.fr>
2025-04-16 14:53:26 +0200

    s

--- tp/avril2025/b1/appliSpring/src/test/java/tp/appliSpring/dao/TestCompteDAO.java
+++ tp/avril2025/b1/appliSpring/src/test/java/tp/appliSpring/dao/TestCompteDAO.java
@@ -1,5 +1,22 @@
 package tp.appliSpring.dao;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import tp.appliSpring.entity.Compte;
+
+@SpringBootTest
 public class TestCompteDAO {
+	
+	@Autowired
+	private CompteDAO compteDAO;//à tester
+	
+	@Test
+	public void testCrudCompte() {
+		Compte cptA = new Compte(null,"compteA",50.0);
+		compteDAO.save(cptA);//sauvegarder le compte en base de données
+		//...
+	}
 
 }
