4f6dbaea3278258696b69b661df48789e8aec131
didier defrance <didier@d-defrance.fr>
2025-04-17 14:51:32 +0200

    s

--- tp/avril2025/a1/appliSpring/src/test/java/tp/appliSpring/dao/TestCompteDao.java
+++ tp/avril2025/a1/appliSpring/src/test/java/tp/appliSpring/dao/TestCompteDao.java
@@ -3,8 +3,11 @@ package tp.appliSpring.dao;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import tp.appliSpring.entity.Compte;
 
 
 @SpringBootTest()
@@ -17,9 +20,14 @@ public class TestCompteDao {
 	
 	Logger logger = LoggerFactory.getLogger(TestCompteDao.class);
 	
+	@Autowired
+	private CompteDao compteDao;//composant à tester
+	
 	@Test
 	public void testDao() {
-		logger.debug("testDao");
+		Compte cA = new Compte(null,"commpteA",50.0);//nouveau compte pas encore sauvegardé
+		cA /* compte sauvegardé */= compteDao.save(cA);
+		logger.debug(cA.toString());
 	}
 
 }
