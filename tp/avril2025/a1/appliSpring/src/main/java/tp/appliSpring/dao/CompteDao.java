479a177242ea408f6ea82ba3f2d1b0ebb1872ee7
didier defrance <didier@d-defrance.fr>
2025-04-17 15:52:03 +0200

    s

--- tp/avril2025/a1/appliSpring/src/main/java/tp/appliSpring/dao/CompteDao.java
+++ tp/avril2025/a1/appliSpring/src/main/java/tp/appliSpring/dao/CompteDao.java
@@ -1,6 +1,9 @@
 package tp.appliSpring.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import tp.appliSpring.entity.Compte;
 
@@ -16,4 +19,11 @@ public interface CompteDao extends JpaRepository<Compte,Long>{
       .deleteById(numero)
       .findAll()
       */
+	
+	//méthode de recherche qui respecte des conventions de nom de méthode findBy...GreaterThanEqual
+	List<Compte> findBySoldeGreaterThanEqual(double soldeMini);
+	
+	@Query("SELECT c FROM Compte c WHERE c.solde >= ?1")
+	List<Compte> findSelonSoldeMini(double soldeMini);
+	
 }
