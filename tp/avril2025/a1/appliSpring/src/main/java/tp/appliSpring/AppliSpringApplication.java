79e97c2064e85e987f77097c92f8aefedd5000b0
didier defrance <didier@d-defrance.fr>
2025-04-17 10:56:08 +0200

    s

--- tp/avril2025/a1/appliSpring/src/main/java/tp/appliSpring/AppliSpringApplication.java
+++ tp/avril2025/a1/appliSpring/src/main/java/tp/appliSpring/AppliSpringApplication.java
@@ -8,6 +8,7 @@ public class AppliSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppliSpringApplication.class, args);
+		System.out.println("http://localhost:8181/appliSpring");
 	}
 
 }
