bc69e27cbe7aac8ae68dd29abe6220e1e81aed89
didier defrance <didier@d-defrance.fr>
2025-04-17 14:26:32 +0200

    Compte

---
+++ tp/avril2025/a1/appliSpring/src/main/java/tp/appliSpring/entity/Compte.java
@@ -0,0 +1,64 @@
+package tp.appliSpring.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Compte {
+	
+	@Id  //pk
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //auto_incr en base remonte en java
+	private Long numero;
+	
+	@Column(length = 64)
+	private String label;
+	
+	private Double solde;
+
+	public Long getNumero() {
+		return numero;
+	}
+
+	public void setNumero(Long numero) {
+		this.numero = numero;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public Double getSolde() {
+		return solde;
+	}
+
+	public void setSolde(Double solde) {
+		this.solde = solde;
+	}
+
+	@Override
+	public String toString() {
+		return "Compte [numero=" + numero + ", label=" + label + ", solde=" + solde + "]";
+	}
+
+	public Compte(Long numero, String label, Double solde) {
+		super();
+		this.numero = numero;
+		this.label = label;
+		this.solde = solde;
+	}
+
+	public Compte() {
+		super();
+	}
+	
+	
+	//+get/set , constructeurs , .toString()
+	
+}
