b1a7143656bf66a10d6b098688550d4e1abee11f
didier defrance <didier@d-defrance.fr>
2025-04-17 11:22:57 +0200

    s

--- tp/avril2025/a1/appliSpring/src/main/java/tp/appliSpring/AppliSpringApplication.java
+++ tp/avril2025/a1/appliSpring/src/main/java/tp/appliSpring/AppliSpringApplication.java
@@ -7,6 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppliSpringApplication {
 
 	public static void main(String[] args) {
+		//activation d'un profile (variante de configuration)
+		//au démarrage de l'application
+		System.setProperty("spring.profiles.active", "mysql");
+		//System.setProperty("spring.profiles.active", "mysql,profileComplementaire2");
 		SpringApplication.run(AppliSpringApplication.class, args);
 		System.out.println("http://localhost:8181/appliSpring");
 	}
