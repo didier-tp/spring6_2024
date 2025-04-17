70fe01db05bd3d61237be2806b1c37a304bbec66
didier defrance <didier@d-defrance.fr>
2025-04-17 11:53:29 +0200

    s

--- tp/avril2025/a1/appliSpring/src/test/java/tp/appliSpring/dao/TestCompteDao.java
+++ tp/avril2025/a1/appliSpring/src/test/java/tp/appliSpring/dao/TestCompteDao.java
@@ -1,8 +1,21 @@
 package tp.appliSpring.dao;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+
+@SpringBootTest()
+//par defaut, @SpringBootTest va utiliser la config automatique de springBoot
+//et le fichier application.properties
+
+//pour tenir compte en plus de application-mysql.properties il faut ajouter @ActiveProfile
+@ActiveProfiles
 public class TestCompteDao {
+	
+	@Test
+	public void testDao() {
+		System.out.println("testDao");
+	}
 
 }
