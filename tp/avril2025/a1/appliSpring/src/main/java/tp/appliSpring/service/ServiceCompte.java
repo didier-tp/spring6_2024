16d7997b2b79cc67f48c5a057cf4920daf43273f
didier defrance <didier@d-defrance.fr>
2025-04-18 09:31:21 +0200

    s

--- tp/avril2025/a1/appliSpring/src/main/java/tp/appliSpring/service/ServiceCompte.java
+++ tp/avril2025/a1/appliSpring/src/main/java/tp/appliSpring/service/ServiceCompte.java
@@ -3,6 +3,7 @@ package tp.appliSpring.service;
 import java.util.List;
 
 import tp.appliSpring.entity.Compte;
+import tp.appliSpring.exception.BankException;
 
 public interface ServiceCompte {
 	Compte searchById(long numeroCompte);
@@ -12,5 +13,5 @@ public interface ServiceCompte {
 	void updateCompte(Compte c);
 	void removeById(long numCompte);
 	
-    void transferer(double montant,long numCptDeb, long numCptCred);
+    void transferer(double montant,long numCptDeb, long numCptCred) throws BankException;
 }
