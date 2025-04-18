9039291fab9c8bea4c360f21bcf3106b45d57b56
didier defrance <didier@d-defrance.fr>
2025-04-18 11:33:22 +0200

    s

--- tp/appliSpringV3/src/main/java/tp/appliSpring/bank/core/service/direct/ServiceOperationDirectImpl.java
+++ tp/appliSpringV3/src/main/java/tp/appliSpring/bank/core/service/direct/ServiceOperationDirectImpl.java
@@ -1,15 +1,18 @@
 package tp.appliSpring.bank.core.service.direct;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import tp.appliSpring.generic.converter.GenericMapper;
+
+import tp.appliSpring.bank.core.model.Operation;
 import tp.appliSpring.bank.core.service.ServiceOperation;
-import tp.appliSpring.bank.persistence.repository.CompteRepository;
-import tp.appliSpring.bank.persistence.repository.OperationRepository;
 import tp.appliSpring.bank.persistence.entity.CompteEntity;
 import tp.appliSpring.bank.persistence.entity.OperationEntity;
-import tp.appliSpring.bank.core.model.Operation;
+import tp.appliSpring.bank.persistence.repository.CompteRepository;
+import tp.appliSpring.bank.persistence.repository.OperationRepository;
+import tp.appliSpring.generic.converter.GenericMapper;
 
 @Service
 @Transactional
@@ -29,5 +32,14 @@ public class ServiceOperationDirectImpl implements ServiceOperation {
         OperationEntity savedOpEntity = daoOperation.save(opEntity);
         op.setNumero(savedOpEntity.getNumero());
         return op;
+    }
+    
+  //@Transactional(readOnly = true)
+    @Transactional()
+    public List<Operation> searchByCompte(long numCpt) {
+        //CompteEntity compteEntity = daoCompte.findWithOperations(numCpt);
+        CompteEntity compteEntity = daoCompte.findById(numCpt).get();
+        compteEntity.getOperations().size();// boucle interne automatique (lazy loading différé)
+        return GenericMapper.MAPPER.map(compteEntity.getOperations(),Operation.class);
     }
 }
