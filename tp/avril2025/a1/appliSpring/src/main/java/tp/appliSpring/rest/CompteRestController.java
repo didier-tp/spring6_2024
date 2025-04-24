2ba8062cd94a8112f398ae428f300e3d12fef819
didier defrance <didier@d-defrance.fr>
2025-04-24 13:55:54 +0200

    post

--- tp/avril2025/a1/appliSpring/src/main/java/tp/appliSpring/rest/CompteRestController.java
+++ tp/avril2025/a1/appliSpring/src/main/java/tp/appliSpring/rest/CompteRestController.java
@@ -1,5 +1,6 @@
 package tp.appliSpring.rest;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,61 +9,78 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import tp.appliSpring.entity.Compte;
 import tp.appliSpring.service.ServiceCompte;
 
-@RestController //composant spring pour WS REST
-@RequestMapping(value="/rest/bank-api/v1/comptes" ,
-               headers="Accept=application/json")
+@RestController // composant spring pour WS REST
+@RequestMapping(value = "/rest/bank-api/v1/comptes", headers = "Accept=application/json")
 public class CompteRestController {
 
 	@Autowired
 	private ServiceCompte serviceCompte;
-	
-	//URL de déclenchement:
-	//http://localhost:8181/appliSpring/rest/bank-api/v1/comptes/1
-	@GetMapping("/{numero}" )
+
+	// URL de déclenchement:
+	// http://localhost:8181/appliSpring/rest/bank-api/v1/comptes/1
+	@GetMapping("/{numero}")
 	public ResponseEntity<Compte> getCompteByNum(@PathVariable("numero") Long numero) {
 		try {
 			Compte c = serviceCompte.searchById(numero);
-			return new ResponseEntity<Compte>(c,HttpStatus.OK);
+			return new ResponseEntity<Compte>(c, HttpStatus.OK);
 		} catch (Exception e) {
 			return new ResponseEntity<Compte>(HttpStatus.NOT_FOUND);
 		}
 	}
 	/*
-	@GetMapping("/{numero}" )
-	public Compte getCompteByNum(@PathVariable("numero") Long numero) {
-		return serviceCompte.searchById(numero);
-	}
-	*/
-	
-	
-	//http://localhost:8181/appliSpring/rest/bank-api/v1/comptes/1
-	@DeleteMapping("/{numero}" )
+	 * @GetMapping("/{numero}" ) public Compte
+	 * getCompteByNum(@PathVariable("numero") Long numero) { return
+	 * serviceCompte.searchById(numero); }
+	 */
+
+	// http://localhost:8181/appliSpring/rest/bank-api/v1/comptes/1
+	@DeleteMapping("/{numero}")
 	public ResponseEntity<?> deleteCompteByNum(@PathVariable("numero") Long numero) {
-			try {
-				serviceCompte.removeById(numero);
-				return new ResponseEntity<>(HttpStatus.NO_CONTENT);//204: OK mais sans détails/ sans message
-			} catch (Exception e) {
-				return new ResponseEntity<Compte>(HttpStatus.NOT_FOUND);
-			}
+		try {
+			serviceCompte.removeById(numero);
+			return new ResponseEntity<>(HttpStatus.NO_CONTENT);// 204: OK mais sans détails/ sans message
+		} catch (Exception e) {
+			return new ResponseEntity<Compte>(HttpStatus.NOT_FOUND);
+		}
 	}
-	
-	//URL de déclenchement:
-	//http://localhost:8181/appliSpring/rest/bank-api/v1/comptes
-	//http://localhost:8181/appliSpring/rest/bank-api/v1/comptes?soldeMini=60
+
+	// URL de déclenchement:
+	// http://localhost:8181/appliSpring/rest/bank-api/v1/comptes
+	// http://localhost:8181/appliSpring/rest/bank-api/v1/comptes?soldeMini=60
 	@GetMapping()
-	public List<Compte> getComptesByCriteria(
-			@RequestParam(value="soldeMini",required=false)Double soldeMini){
-		if(soldeMini!=null)
+	public List<Compte> getComptesByCriteria(@RequestParam(value = "soldeMini", required = false) Double soldeMini) {
+		if (soldeMini != null)
 			return serviceCompte.searchBySoldeMini(soldeMini);
-		/*else*/
+		/* else */
 		return serviceCompte.searchAll();
 	}
-	
+
+	// URL de déclenchement (en mode POST)
+	// http://localhost:8181/appliSpring/rest/bank-api/v1/comptes
+	//avec { "numero" : null , "label" : "nouveau_compte" , "solde" : 50.0 }
+	@PostMapping()
+	public ResponseEntity<?> postCompte(/* @Valid */ @RequestBody Compte obj) {
+		Compte savedObj = serviceCompte.insertCompte(obj); // avec id auto_incrémenté
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(savedObj.getNumero()).toUri();
+		// return ResponseEntity.created(location).build();
+		// return 201/CREATED , no body but URI to find added obj
+		return ResponseEntity.created(location).body(savedObj);
+		// return 201/CREATED with savedObj AND with URI to find added obj
+		/*
+		 * ou bien encore return ResponseEntity.ok()
+		 * .headers(responseHeadersWithLocation).body(savedObj);
+		 */
+	}
+
 }
