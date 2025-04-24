5422af5d5bbdec67c474dcdb444f0f6a4d0f8bec
didier defrance <didier@d-defrance.fr>
2025-04-24 11:05:58 +0200

    soldeMini

--- tp/avril2025/a1/appliSpring/src/main/java/tp/appliSpring/rest/CompteRestController.java
+++ tp/avril2025/a1/appliSpring/src/main/java/tp/appliSpring/rest/CompteRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import tp.appliSpring.entity.Compte;
@@ -20,8 +21,13 @@ public class CompteRestController {
 	
 	//URL de déclenchement:
 	//http://localhost:8181/appliSpring/rest/bank-api/v1/comptes
+	//http://localhost:8181/appliSpring/rest/bank-api/v1/comptes?soldeMini=60
 	@GetMapping()
-	public List<Compte> getComptesByCriteria(){
+	public List<Compte> getComptesByCriteria(
+			@RequestParam(value="soldeMini",required=false)Double soldeMini){
+		if(soldeMini!=null)
+			return serviceCompte.searchBySoldeMini(soldeMini);
+		/*else*/
 		return serviceCompte.searchAll();
 	}
 	
