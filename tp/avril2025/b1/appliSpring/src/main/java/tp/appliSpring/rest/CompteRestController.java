d5af52361d29890d680d4411cedf20fc38672fc0
didier Defrance <didier@d-defrance.fr>
2025-04-25 09:23:33 +0200

    Responseentity

--- tp/avril2025/b1/appliSpring/src/main/java/tp/appliSpring/rest/CompteRestController.java
+++ tp/avril2025/b1/appliSpring/src/main/java/tp/appliSpring/rest/CompteRestController.java
@@ -3,6 +3,8 @@ package tp.appliSpring.rest;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,9 +25,18 @@ public class CompteRestController {
 	//URL de déclenchement:
 	//http://localhost:8181/appliSpring/rest/bank-api/v1/comptes/1
 	@GetMapping("/{numero}") 
+	public ResponseEntity<Compte> getCompteByNum(@PathVariable("numero")long numero) {
+		try {
+			return new ResponseEntity<Compte>(serviceCompte.searchByNumero(numero),HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<Compte>(HttpStatus.NOT_FOUND);//404
+		}
+	}
+	/*
+	@GetMapping("/{numero}") 
 	public Compte getCompteByNum(@PathVariable("numero")long numero) {
 		return serviceCompte.searchByNumero(numero);
-	}
+	}*/
 	
 	//URL de déclenchement:
 	//http://localhost:8181/appliSpring/rest/bank-api/v1/comptes
